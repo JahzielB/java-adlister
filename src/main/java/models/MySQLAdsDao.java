25d4ac7c94cd6c4d5456acf840862e07b654af34
JahzielB <jahzielb@gmail.com>
2020-07-21 15:06:38 -0500

    finished jdbc exercise

---
+++ src/main/java/models/MySQLAdsDao.java
@@ -0,0 +1,64 @@
+package models;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads{
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> ads = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+            while (rs.next()) {
+                ads.add(new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ads;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        long lastInsertedId = 0;
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(String.format("INSERT INTO ads (user_id, title, description) VALUES ('%d', '%s', '%s')",
+                        ad.getUserId(),
+                        ad.getTitle(),
+                        ad.getDescription()
+                    ));
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            lastInsertedId = rs.getLong(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lastInsertedId;
+    }
+}
